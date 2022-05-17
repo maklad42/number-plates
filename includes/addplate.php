9368ec779be015282cf8baf64742343c9c4a641f
maklad42 <maklad42+git@gmail.com>
2022-05-17 22:30:58 +0900

    Adds saving of the updated cars.json file, as well as creating a backup of the file before updating. 220517

--- includes/addplate.php
+++ includes/addplate.php
@@ -10,7 +10,23 @@ if (isset($_REQUEST['plate'])) : ?>
   if ($plates[$n]['found'] == 'true') {
     $status = 'Sorry, ' . $n . ' is already in the DB.';
   } else {
-    $status = 'Added!';
+    // set date
+    $dt = date('Y-m-d');
+
+    // create backup file
+    $bak = json_encode($plates, JSON_FORCE_OBJECT | JSON_PRETTY_PRINT);
+    file_put_contents('../data/bak/cars_' . $dt . '.json', $bak);
+
+    // update plates list
+    $plates[$n]['found'] = 'true';
+    $plates[$n]['when'] = $dt;
+
+    // create new file
+    $newfile = json_encode($plates, JSON_FORCE_OBJECT | JSON_PRETTY_PRINT);
+    file_put_contents('../data/cars.json', $newfile);
+
+    // display the status of the request
+    $status = 'Added ' . $n . ' on ' . $dt;
   }
   ?>
 
