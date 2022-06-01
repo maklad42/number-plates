45223f100e1660ac78952ac4e1d0b643ead62a22
maklad42 <maklad42+git@gmail.com>
2022-06-01 22:26:41 +0900

    Adds number of cars found in total and in the current block as well as the number left to find. Fixes issue #9. 220601

--- includes/blocks.php
+++ includes/blocks.php
@@ -6,12 +6,30 @@
     $offset = 0;
   }
 
+  $start = $offset * 1000;
+  $end = $offset * 1000 + 999;
+
   $json = file_get_contents('../data/cars.json');
   $plates = json_decode($json, true);
 
+  $total_found = 0;
+  $found_here = 0;
+
+  foreach ($plates as $key => $val) {
+    if ($val['found'] === 'true') {
+      $total_found += 1;
+    }
+    if ($key >= $start && $key <= $end) {
+      if ($val['found'] === 'true') {
+        $found_here += 1;
+      }
+    }
+  }
+
   ?>
 
-  <div class="title"><?php echo $offset * 1000; ?> ~ <?php echo $offset * 1000 + 999; ?></div>
+  <div class="title"><?php echo $start; ?> ~ <?php echo $end; ?></div>
+  <div class="totalfound"><?php echo $found_here; ?> out of the <?php echo $total_found; ?> total found plates. Only <?php echo number_format(10000 - $total_found); ?> to go.</div>
   <div class="numblock-outer">
     <?php for ($i = 0; $i < 10; $i++) : ?>
       <div class="numblock" data-offset="<?php echo $offset; ?>">
